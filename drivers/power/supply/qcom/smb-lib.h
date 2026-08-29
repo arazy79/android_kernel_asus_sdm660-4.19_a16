3d44699851b2d94d3f7041f4c743720a33eda1a6
arazy79 <53860204+arazy79@users.noreply.github.com>
2026-08-29 02:35:00 +0100

    Update smb-lib.h

--- drivers/power/supply/qcom/smb-lib.h
+++ drivers/power/supply/qcom/smb-lib.h
@@ -71,7 +71,7 @@ enum print_reason {
 #define VCONN_MAX_ATTEMPTS	3
 #define OTG_MAX_ATTEMPTS	3
 #define BOOST_BACK_STORM_COUNT	3
-#define WEAK_CHG_STORM_COUNT	24
+#define WEAK_CHG_STORM_COUNT	8
 
 enum smb_mode {
 	PARALLEL_MASTER = 0,
